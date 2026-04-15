b6fd24e69f2abd75a4282a15baf8412a70591710
vpanagos <panos@kioski.gr>
2026-04-15 13:38:56 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 # bootstrap.sh
 
 # 1. Update package manager and install prerequisites (Debian/Ubuntu example)
+dnf -y install epel-release
 dnf -y update
-dnf -y install git ansible epel-release
+dnf -y install git ansible-core
 
 sudo ansible-pull -U https://github.com/uowmnoc/workstation-config.git
