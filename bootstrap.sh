81a392c3ace45c2afe0fc23c310b102e5f4e8c19
vpanagos <panos@kioski.gr>
2026-04-16 10:24:18 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# bootstrap.sh
 
-# 1. Update package manager and install prerequisites (Debian/Ubuntu example)
 dnf -y install epel-release
-dnf -y update
+cat dnf -y update
 dnf -y install git ansible-core
+dnf -y sssd-common oddjob oddjob-mkhomedir sssd-ad adcli samba-common-tools krb-workstation
+
+realm -v discover admin.uowm.gr
+realm -v -U noc join admin.uowm.gr
 
 sudo ansible-pull -U https://github.com/uowmnoc/workstation-config.git
