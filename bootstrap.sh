4b44511e2d808ccbf216a5a1dc3a538d5e0977cc
vpanagos <panos@kioski.gr>
2026-04-17 09:12:35 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,17 @@
 
 dnf -y install epel-release
 cat dnf -y update
+
+dnf config-manager --set-enabled crb
+
+dnf -y install cinnamon cinnamon-calendar-server cinnamon-control-center \
+cinnamon-control-center-filesystem cinnamon-desktop cinnamon-menus \
+cinnamon-screensaver cinnamon-session cinnamon-settings-daemon cinnamon-themes \
+cinnamon-translations slick-greeter-cinnamon xorg-x11* lightdm-settings gnome-terminal
+
+systemctl enable lightdm
+systemctl disable gdm
+
 dnf -y install git ansible-core
 dnf -y sssd-common oddjob oddjob-mkhomedir sssd-ad adcli samba-common-tools krb-workstation
 
